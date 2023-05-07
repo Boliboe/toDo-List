90e107c49fd02b3428471bed616ef93225367d7e
Boliboe <iamjohnsomto@gmail.com>
2023-05-07 09:48:19 +0100

    changed the delete icons from svg to i.class icons

--- todos.js
+++ todos.js
@@ -7,7 +7,7 @@ const generateTemplate = todo =>{
     const html = `
         <li class="list-group-item d-flex justify-content-between align-items-center text-light">
         <span>${todo}</span>
-        <svg class="w-5 ml-2 delete" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+        <i class="ri-delete-bin-5-line delete"></i>
         </li>`;
 
         list.innerHTML += html;
